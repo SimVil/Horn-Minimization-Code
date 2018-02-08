75cf99e40a5b6ea8f3b3eb9e3dd7603604ada8eb
Unknown <simon.vilmin@gmail.com>
2018-02-09 00:56:25 +0300

    Added some files, some docs to test.
    Maier Algorithm passes all tests. Tests are (in a way) documented. Prepare to merge on master as a MVP for Maier Algorithm.

--- Tests/Test_Maier_Unit.cpp
+++ Tests/Test_Maier_Unit.cpp
@@ -54,54 +54,97 @@ TEST_CASE("Maier Algorithm: empty basis")
 /// The input will be implicational basis with contradictions. Contradictions are implications A --> {}. The algorithm
 /// should remove them because they are redundant:
 ///     - Input: L with implication(s) A --> {}
-///     - Expceted output:
+///     - Expected output:
 ///         -# irredundancy: L_c with contradiction removed
 ///         -# maier minimization: a minimal basis.
 /// \note For this test, we use automated file testing. For each basis we have 3 files (input, inter, output) containing
 /// respectively the original input, the basis after redundancy elimination, the expected minimum basis. Using a reader
 /// we can check for equivalence after each steps.
-//TEST_CASE("Maier Algorithm: contradictions")
-//{
-//    std::string path = "D:/Documents/Courses/Master Thesis/Code/Algorithms/Tests/Contradictions/";
-//    std::string extension = ".txt";
-//
-//    unsigned int tests_nb = 2;  // number of contradictions cases to run.
-//    unsigned int seq_len = 3;  // input, inter, output
-//    std::vector<std::string> filenames = {"input", "inter", "output"};
-//
-//    SequenceTesting(path, extension, filenames, tests_nb);
-//
-//}
+TEST_CASE("Maier Algorithm: contradictions")
+{
+    std::string path = "D:/Documents/Courses/Master Thesis/Code/Algorithms/Tests/Contradictions/";
+    std::string extension = ".txt";
+
+    unsigned int tests_nb = 2;  // number of contradictions cases to run.
+    unsigned int seq_len = 3;  // input, inter, output
+    std::vector<std::string> filenames = {"input", "inter", "output"};
+
+    std::cout << "Test Case: Contradictions" << std::endl << std::endl;
+    SequenceTesting(path, extension, filenames, tests_nb);
+
+}
 
 
+/// \brief Test of basis with non-closed empty-set.
+///
+/// The input is a basis with some {} --> A implications. This should increase redundancy and direct
+/// determination probability:
+///     - Input: L with implications {} --> A
+///  We just expect irredundant and minimal basis as results.
+TEST_CASE("Maier Algorithm: non-closed empty set")
+{
+    std::string path = "D:/Documents/Courses/Master Thesis/Code/Algorithms/Tests/NonClosedEmptySet/";
+    std::string extension = ".txt";
 
-//TEST_CASE("Maier Algorithm: non-closed empty set")
-//{
-//    std::string path = "D:/Documents/Courses/Master Thesis/Code/Algorithms/Tests/NonClosedEmptySet/";
-//    std::string extension = ".txt";
-//
-//    unsigned int tests_nb = 3;
-//    unsigned int seq_len = 3;
-//    std::vector<std::string> filenames = {"input", "inter", "output"};
-//
-//    SequenceTesting(path, extension, filenames, tests_nb);
-//}
+    unsigned int tests_nb = 3;
+    unsigned int seq_len = 3;
+    std::vector<std::string> filenames = {"input", "inter", "output"};
+
+    std::cout << "Test Case: Non-closed Emptyset" << std::endl << std::endl;
+    SequenceTesting(path, extension, filenames, tests_nb);
+}
 
 
+/// \brief Test of "standard" basis.
+///
+/// Input is a basis randomly generated (reduced).
 TEST_CASE("Maier Algorithm: some standard tests")
 {
     std::string path = "D:/Documents/Courses/Master Thesis/Code/Algorithms/Tests/Standard/";
+    std::string extension = ".txt";
+
+    unsigned int tests_nb = 3;
+    unsigned int seq_len = 3;
+    std::vector<std::string> filenames = {"input", "inter", "output"};
+
+    std::cout << "Test Case: Standard cases" << std::endl << std::endl;
+
+    SequenceTesting(path, extension, filenames, tests_nb);
+}
+
+
+/// \brief Test of non-reduced form.
+///
+/// The input is a basis with implications A --> B, such that A n B != {} (possibly).
+/// The algorithm is expected to work the same way as usual.
+TEST_CASE("Maier Algorithm: Non-reduced form")
+{
+    std::string path = "D:/Documents/Courses/Master Thesis/Code/Algorithms/Tests/NonReduced/";
     std::string extension = ".txt";
 
     unsigned int tests_nb = 1;
     unsigned int seq_len = 3;
     std::vector<std::string> filenames = {"input", "inter", "output"};
 
+    std::cout << "Test Case: Non-Reduced cases" << std::endl << std::endl;
+
     SequenceTesting(path, extension, filenames, tests_nb);
 }
 
 
-
+/// \brief summarizes a test sequence over tests files.
+///
+/// The function reads a list of file with some implication basis to test. It
+/// corresponds in a way to scenario. The first file should be an input file, the
+/// input basis. Consequent files should be results according to some tests going
+/// to be done.
+///
+/// TODO: rewrite to discard indices (basis[0], basis[1], basis[2]).
+///
+/// \param [in] path path of the files.
+/// \param [in] extension extension of the files (e.g: txt)
+/// \param [in] filenames the sequence of files to read (~scenario)
+/// \param [in] tests_nb number of sequence to treat.
 void SequenceTesting(const std::string &path, const std::string &extension, const std::vector<std::string> &filenames,
                      const unsigned &tests_nb )
 {
@@ -137,6 +180,15 @@ void SequenceTesting(const std::string &path, const std::string &extension, cons
         buffL = FCA::Convert(minL, sigma_s);
 
         REQUIRE(IsVectorOfImplicationsIdentical(buffL, basis[2]));
+
+        std::cout << "---- FILE: " << i << " ----" << std::endl;
+        std::cout << "Input:" << std::endl;
+        PrintImplications(std::cout, basis[0]);
+        std::cout << "Expected:" << std::endl;
+        PrintImplications(std::cout, basis[2]);
+        std::cout << "Output:" << std::endl;
+        PrintImplications(std::cout, buffL);
+        std::cout << "---- ==== ----" << std::endl;
 
         for(int j = 0; j < seq_len; ++j)
         {
