96063242c6f802da6d57059062d5d209ab6cfa7e
Unknown <simon.vilmin@gmail.com>
2018-03-06 15:04:47 +0300

    Added Comments/Doc
    Added documentation and comments to the class FDGraph and its member functions. Superfluousness still not finished.

--- CanonicalBasis/graph_FDgraph.cpp
+++ CanonicalBasis/graph_FDgraph.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 
 
+/// \brief builds a graph out of an implication system.
 GRAPH::FDGraph::FDGraph(const std::vector<FCA::ImplicationInd> &L) {
     size_t implNum = L.size();
     size_t attrNum = L.front().Premise().size();
@@ -14,11 +15,12 @@ GRAPH::FDGraph::FDGraph(const std::vector<FCA::ImplicationInd> &L) {
     FCA::BitSet premise(attrNum);
     FCA::BitSet conclusion(attrNum);
 
+    // vector aims to limit complexity, because access is O(1).
     std::vector<elt_t *> simple_vertex(attrNum);
     elt_t *node;
 
 
-    // O(|Sigma|)
+    // for each attribute, a simple node.
     for (size_t i = 0; i < attrNum; ++i)
     {
         vertex.set(i);
@@ -29,6 +31,8 @@ GRAPH::FDGraph::FDGraph(const std::vector<FCA::ImplicationInd> &L) {
 
     // O(|B| x |Sg|) + O(|B| x (|B| + |Sigma|)) + O(|B| x |Sg|)
     // the second stands for finding a vertex.
+    // for each body, add a node, if it is compound, add dotted arcs and fill D
+    // lists.
     for (size_t i = 0; i < implNum; ++i)
     {
         premise = L[i].Premise();
@@ -42,6 +46,12 @@ GRAPH::FDGraph::FDGraph(const std::vector<FCA::ImplicationInd> &L) {
             {
                 if(premise.test(j))
                 {
+                    // having simple_vertex helps to access an element of the
+                    // list in O(1), preserving simple complexity, with a linear
+                    // space augmentation (with respect to the attribute set)
+
+                    // dotted arc from abc to a
+                    // a belongs to D, hence arc from a to abc in D_a.
                     vertex.set(j);
                     graph.back().second.edges["dotted"].push_back(simple_vertex[j]);
                     simple_vertex[j]->second.edges["D"].push_back(&(graph.back()));
@@ -51,6 +61,7 @@ GRAPH::FDGraph::FDGraph(const std::vector<FCA::ImplicationInd> &L) {
             }
         }
 
+        // findVertex is O(|B| + |Sigma|)
         node = findVertex(premise);
         for (size_t j = 0; j < attrNum; ++j)
         {
@@ -65,6 +76,7 @@ GRAPH::FDGraph::FDGraph(const std::vector<FCA::ImplicationInd> &L) {
 
 }
 
+/// finds a node given its name.
 elt_t *GRAPH::FDGraph::findVertex(const FCA::BitSet &u) {
     auto it = graph.begin();
     elt_t * r = nullptr;
@@ -79,6 +91,7 @@ elt_t *GRAPH::FDGraph::findVertex(const FCA::BitSet &u) {
     return r;
 }
 
+/// computes the closure of a node in the graph.
 void
 GRAPH::FDGraph::NodeClosure(elt_t *i, const std::list<elt_t *> &L_i, const std::string &type)
 {
@@ -88,10 +101,15 @@ GRAPH::FDGraph::NodeClosure(elt_t *i, const std::list<elt_t *> &L_i, const std::
     std::string dotted_closure = "dotted+";
     bool found;
 
+    // S_i is the list of nodes we can reach from i.
+
     while (!S_i.empty())
     {
         j = S_i.front();
         S_i.pop_front();
+
+        // j simple node, check for all compound node of D_j
+        // to see if union rule must be applied
 
         if(j->first.count() == 1)
         {
@@ -100,6 +118,8 @@ GRAPH::FDGraph::NodeClosure(elt_t *i, const std::list<elt_t *> &L_i, const std::
                 if(it->first != i->first)
                     it->second.counter++;
 
+                // if counter(v) == |v|, then we can reach v from i by union
+                // because we got all its components.
                 if(it->second.counter == it->first.count())
                     S_i.push_back(it);
             }
@@ -108,16 +128,19 @@ GRAPH::FDGraph::NodeClosure(elt_t *i, const std::list<elt_t *> &L_i, const std::
         i->second.edges[closure_type].push_back(j);
 
         // we try to perform the union of edges by splicing (constant time operation)
-        // thus avoiding duplication of the loop.
-
+        // thus avoiding duplication of the next for loop.
         std::list<elt_t *> full = j->second.edges["full"];
         std::list<elt_t *> dotted = j->second.edges["dotted"];
         full.splice(full.end(), dotted);
 
+        // for each node k with an ingoing arc from j
+        // if k is neither in the closure we are computing, nor the dotted closure
+        // it must be treated, by transitivity.
         for(auto it : full)
         {
             found = it->first == i->first;
             auto dotted_it = i->second.edges[dotted_closure].begin();
+
 
             while (!found && dotted_it != i->second.edges[dotted_closure].end())
             {
@@ -125,6 +148,8 @@ GRAPH::FDGraph::NodeClosure(elt_t *i, const std::list<elt_t *> &L_i, const std::
                 ++dotted_it;
             }
 
+            // if closure_type = dotted, we don't need to run over closure_type
+            // a second time.
             if (!found && dotted_closure != closure_type)
             {
                 auto full_it = i->second.edges[closure_type].begin();
@@ -135,6 +160,8 @@ GRAPH::FDGraph::NodeClosure(elt_t *i, const std::list<elt_t *> &L_i, const std::
                 }
             }
 
+            // if k is not in any closure, we can reach it from i, and must treat
+            // it later.
             if (!found) { S_i.push_back(it); }
         }
 
@@ -142,11 +169,16 @@ GRAPH::FDGraph::NodeClosure(elt_t *i, const std::list<elt_t *> &L_i, const std::
 
 }
 
-
+/// searches the graph to remove edges to some node.
 void GRAPH::FDGraph::removeEdgestoVertex(elt_t &v, const std::vector<std::string> &lists) {
     size_t size;
     unsigned int i = 0;
     elt_t *elt;
+
+    // we have to look in edges given by lists, for all vertices.
+    // warning: using a pop and push strategy is valid here, since we have a fixed number
+    // of elements to treat (we don't depend on the end() iterator which can be modified
+    // by deletion).
 
     for (auto &vertex_it : graph)
     {
@@ -159,6 +191,8 @@ void GRAPH::FDGraph::removeEdgestoVertex(elt_t &v, const std::vector<std::string
                 elt = vertex_it.second.edges[s].front();
                 vertex_it.second.edges[s].pop_front();
 
+                // if the edges (vertex_it, elt) is not (vertex_t, i), we must
+                // keep it, hence we push it back in the edge list.
                 if(elt->first != v.first)
                     vertex_it.second.edges[s].push_back(elt);
 
@@ -170,6 +204,7 @@ void GRAPH::FDGraph::removeEdgestoVertex(elt_t &v, const std::vector<std::string
 
 }
 
+/// removes redundant nodes.
 void GRAPH::FDGraph::RedundancyElimination() {
     std::vector<std::string> lists = {"dotted+", "full+", "D"};
 
@@ -183,6 +218,10 @@ void GRAPH::FDGraph::RedundancyElimination() {
         tmp = it;
         ++it;
 
+        // setting the condition here prevents segmentation fault
+        // because erasing an element can alter the end() iterator.
+        // Thus, the it variable could be past the end, but not equal to the new
+        // graph.end() after destruction of an element.
         stop = (it == graph.end());
 
         if (tmp->first.count() != 1 && tmp->second.edges["full+"].empty())
@@ -193,6 +232,7 @@ void GRAPH::FDGraph::RedundancyElimination() {
     }
 }
 
+/// converts a graph to an implication basis.
 void GRAPH::FDGraph::Convert(std::vector<FCA::ImplicationInd> &L, const std::string &type) {
     if (graph.empty())
         return;
@@ -204,14 +244,14 @@ void GRAPH::FDGraph::Convert(std::vector<FCA::ImplicationInd> &L, const std::str
     FCA::BitSet premise(attrNum);
     FCA::BitSet conclusion(attrNum);
 
-    for(auto &it : graph)
+    for(auto &vertex : graph)
     {
-        premise = it.first;
+        premise = vertex.first;
         conclusion.reset();
 
-        for(auto &it_full : it.second.edges[type])
+        for(auto &edge : vertex.second.edges[type])
         {
-            conclusion |= it_full->first;
+            conclusion |= edge->first;
         }
 
         if (!conclusion.none())
@@ -220,6 +260,8 @@ void GRAPH::FDGraph::Convert(std::vector<FCA::ImplicationInd> &L, const std::str
 
 }
 
+
+/// computes the closure of the graph.
 void GRAPH::FDGraph::Closure()
 {
     std::list<elt_t *> tmp;
@@ -230,38 +272,42 @@ void GRAPH::FDGraph::Closure()
     // necessary for these special iterator since we iterate over pointers, thus the pointed value is not altered
     // by copying a pointer (m, f_it).
 
-    for(auto &it : graph)
+    for(auto &vertex : graph)
     {
-        for (auto &it_b : graph)
-            it_b.second.counter = 0;
+        for (auto &vertex_reset : graph)
+            vertex_reset.second.counter = 0;
 
-        if (!(it.second.edges["dotted"].empty() && it.second.edges["full"].empty()))
+        if (!(vertex.second.edges["dotted"].empty() && vertex.second.edges["full"].empty()))
         {
-            for (auto m : it.second.edges["D"])
+            for (auto m : vertex.second.edges["D"])
             {
                 m->second.counter = 1;
             }
 
-            if(it.first.count() != 1)
-                NodeClosure(&it, it.second.edges["dotted"], "dotted");
+            if(vertex.first.count() != 1)
+                NodeClosure(&vertex, vertex.second.edges["dotted"], "dotted");
 
-            for (const auto f_it : it.second.edges["full"])
+            // this loop determines which nodes must be treated when computing full closure
+            // we perform (for a given vertex v):
+            //      L_v = v["full"] - v["dotted+"]
+            for (const auto full_edge : vertex.second.edges["full"])
             {
-                auto d_it = it.second.edges["dotted+"].begin();
+                auto closed_dotted_edge = vertex.second.edges["dotted+"].begin();
                 found = false;
 
-                while (!found && d_it != it.second.edges["dotted+"].end())
+
+                while (!found && closed_dotted_edge != vertex.second.edges["dotted+"].end())
                 {
-                    found |= f_it->first == (*d_it)->first;
-                    ++d_it;
+                    found |= full_edge->first == (*closed_dotted_edge)->first;
+                    ++closed_dotted_edge;
                 }
 
                 if (!found)
-                    tmp.push_back(f_it);
+                    tmp.push_back(full_edge);
 
             }
 
-            NodeClosure(&it, tmp, "full");
+            NodeClosure(&vertex, tmp, "full");
             tmp.clear();
         }
     }
@@ -288,7 +334,7 @@ void GRAPH::FDGraph::SuperfluousnessElimination() {
             // if i is superfluous
             if (j != adj_v.end())
             {
-                
+
                 removeEdgestoVertex(*i, {"dotted+", "full+"});
             }
         }
--- CanonicalBasis/graph_FDgraph.h
+++ CanonicalBasis/graph_FDgraph.h
@@ -1,6 +1,14 @@
-//
-// Created by simon on 12.02.18.
-//
+/// \file graph_FDgraph.h
+/// \author Simon Vilmin
+/// \date 2018
+///
+/// \brief defines FD-Graph class (FD = Functional Dependency)
+///
+/// The file contains an implementation of the structure given by Ausiello and al. for representing functional
+/// dependency basis (hence implication system). An adjacency-list based representation is adopted to stick to
+/// the theoretical work. Here are the two main papers we rely on:
+///     - "Graph Algorithms for Functional Dependency Manipulation" (Ausiello and al., 1983, ACM J.)
+///     - "Minimal Representation of Directed Hypergraphs" (Ausiello and al., 1986, SIAM J. Computer Science)
 
 #ifndef ALGORITHMS_GRAPH_FDGRAPH_H
 #define ALGORITHMS_GRAPH_FDGRAPH_H
@@ -12,11 +20,30 @@
 #include "fca_datastructures.h"
 
 
+
+/// see struct vertex
 typedef struct vertex vertex_t;
 
+
+/// \typedef elt_t represents an element within a list. We store a BitSet for the label (name of the node) and an
+/// vertex_t (see struct vertex).
 typedef std::pair<FCA::BitSet, vertex_t> elt_t;
 
+
+/// \struct defines a vertex in an FD-graph.
+///
+/// A node in an FD-graph can be either simple (an attribute) or a compound node (body of implication). Out of each
+/// vertex, we can either have \a full or \a dotted arcs. Hence, we provide a map of edges type. Since map provide
+/// access in logarithmic time (with respect to the number of list) and the number of lists being constant (up to 5,
+/// see class FDGraph documentation), choosing a (key, element) setting allow for very little overhead in computation
+/// as much as easier to understand coding.
+///
+/// \param edges map of various edges (e.g: dotted, full, ...). Keys are string naming the edge type, and elements are
+///        lists of pointers on elt_t's, avoiding duplication of BitSets, while preserving easy access to adjacent node
+///        labels and arcs.
+/// \param counter helps to keep track of treated components of a coumpound node.
 struct vertex{
+
     std::map<std::string, std::list<elt_t *>> edges;
     unsigned int counter;
 
@@ -24,29 +51,109 @@ struct vertex{
     explicit vertex(unsigned int q) : counter(q) {};
 };
 
+
+
 namespace GRAPH
 {
+    /// \class represents an FD-Graph and its closure.
+    ///
+    /// This class defines an FD-Graph out of an implication system. Here is a quick recall of the structure from an
+    /// example. Let L = (abc --> d) be the implication basis to model as an FD-Graph. Each attribute of L will be
+    /// a \a simple node. Each body of L will be a \c compound node, here abc. Then, for a compound node, we draw a
+    /// \a dotted arc from this node to each of this component. Finally, for each body, we draw a \a full arc from
+    /// the related node to each of the attribute of the head. In our case, we will eventually obtain:
+    ///     - simple nodes: a, b, c, d
+    ///     - compound nodes: abc
+    ///     - full arcs: (abc, d)
+    ///     - dotted arcs: (abc, a), (abc, b), (abc, c)
+    /// Then, in order to limit space complexity, and knowing that we won't add new nodes when computing the closure
+    /// of a Graph, the closure edges will be embedded in the graph structure. This avoids duplicating BitSets and
+    /// vertices, for the price of a very little overhead in accessing edges lists in vertex_t element (constant
+    /// logarithmic time). The possible types of edges are then:
+    ///     - full
+    ///     - dotted
+    ///     - full+: full arcs in the closure of the graph
+    ///     - dotted+: dotted arcs in the closure
+    ///     - D: arcs we need to compute the closure. They link attributes to compound node they belong to
+    ///         (in L, we will have D_a = (a, abc)) for instance.
+    /// The size of a graph is O(|L|) up to a constant factor.
     class FDGraph
     {
     protected:
 
+        /// \brief the graph structure.
         std::list<elt_t> graph;
+
+        /// \brief computes the closure of a node in the graph.
+        ///
+        /// The main point is to get, given a node, all the nodes we can reach by transitivity or by union.
+        /// Transitivity is, given nodes i, j, k, having edges (i, j) and (j, k), adding (i, k) to the closure
+        /// of i. Union is, given a node i, and a compound node j = j1 j2 j3..., if we have (i, j_k) for all
+        /// j_k in j in the closure, then (i, j) will be also.
+        ///
+        /// \param v the element to compute the closure of.
+        /// \param L_i initial list of nodes to treat.
+        /// \param type type of closure, dotted or full.
         void NodeClosure(elt_t *v, const std::list<elt_t *> &L_i, const std::string &type);
 
+        /// \brief searches the graph to remove edges to some node.
+        ///
+        /// Given a node v, we are going to run over the graph to remove all arcs (u, v). This is helpful
+        /// for removing node during redundancy elimination, but also to isolate a vertex within superfluousness
+        /// elimination without being compelled to remove it.
+        ///
+        /// \param v the node to remove
+        /// \param lists the lists from which we might remove (u, v) edges.
         void removeEdgestoVertex(elt_t &v, const std::vector<std::string> &lists);
 
+        /// \brief finds a node given its name.
+        ///
+        /// \param u the nme of the node (in terms of set) we want to find
+        /// \return a pointer on the corresponding element.
         elt_t *findVertex(const FCA::BitSet &u);
 
     public:
+
+        /// \brief default constructor;
         FDGraph() = default;
+
+        /// \brief builds a graph out of an implication system.
+        ///
+        /// \param L implication system.
         explicit FDGraph(const std::vector<FCA::ImplicationInd> &L);
 
+        /// \brief removes redundant nodes.
+        ///
+        /// \a requires the closure to be built. The principle is the following
+        /// \code
+        ///     Remove redundant nodes
+        ///     Requires closure to be computed
+        ///
+        ///     For each compound node v:
+        ///         if v has no arcs in full+:
+        ///             remove edges to vertex v
+        ///             remove vertex v from graph
+        ///
+        /// \endcode
         void RedundancyElimination();
 
+        /// \brief remove superfluous nodes.
         void SuperfluousnessElimination();
 
+        /// \brief converts a graph to an implication basis.
+        ///
+        /// Usually, we will need to get implications out of full arcs, but in some cases, one would get
+        /// other edges to perform testing. Thus, we provide a degree of freedom on the implications we can build.
+        ///
+        /// \param L the resulting list.
+        /// \param type the edges we would like to consider.
         void Convert(std::vector<FCA::ImplicationInd> &L, const std::string &type = "full");
 
+        /// \brief computes the closure of the graph.
+        ///
+        /// This function fills two lists for each node: dotted+ and full+ for dotted closure and full closure
+        /// respectively. Because dotted arcs are more likely to denote a node to be removed, we compute dotted
+        /// closure first.
         void Closure();
 
 
