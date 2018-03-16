887a6ca4b5560f96a15479607c7ec6c0fce1ba28
Unknown <simon.vilmin@gmail.com>
2018-03-16 11:35:50 +0300

    Lectic Algorithm
    Coded lectic order and lectic algorithm.

---
+++ CanonicalBasis/horn_lectic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Simon on 16/03/2018.
+//
+
+#include "horn_lectic.h"
---
+++ CanonicalBasis/horn_lectic.h
@@ -0,0 +1,14 @@
+//
+// Created by Simon on 16/03/2018.
+//
+
+#ifndef ALGORITHMS_HORN_LECTIC_H
+#define ALGORITHMS_HORN_LECTIC_H
+
+
+class horn_lectic {
+
+};
+
+
+#endif //ALGORITHMS_HORN_LECTIC_H
